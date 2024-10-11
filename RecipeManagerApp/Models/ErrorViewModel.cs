2ee00c3387174b80e30af6e9f2e74d0f8d22d03a
ZainabM872 <zainabmajid2408@gmail.com>
2024-10-11 00:23:51 -0600

    Initial commit for RecipeManagerApp

---
+++ RecipeManagerApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RecipeManagerApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
